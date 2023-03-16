de5aed36a895c74a261daaff2c4ece535667c068
Jose <joseluis0528@gmail.com>
2023-03-16 05:52:21 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 // Code your solution in this file!
 
 const returnFirstTwoDrivers = function(array) {
-    const newArr = array.slice(0, 2);
-    return newArr; 
+    return array.slice(0, 2);
 }
 
 const returnLastTwoDrivers = function(array) {
-    const newArr = array.slice(2);
-    return newArr;
+    return array.slice(2);
 }
 
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
